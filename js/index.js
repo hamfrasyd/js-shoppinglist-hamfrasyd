3ecf2ca6c8f7eaa4093ed6852e52b3677038cfbe
Kevin <68694478+Mbuzinous@users.noreply.github.com>
2025-03-19 10:45:27 +0100

    - Create a function that return a new Li element - Create a function that append a Li element to a list

--- js/index.js
+++ js/index.js
@@ -1,2 +1,20 @@
 //Write your Javascript code here
-console.log("Shoppinglist")
+function addLi(classAttribute, IdAttribute, paramTextNode){
+    
+    let newElement = document.createElement("li");
+
+    newElement.setAttribute("class", classAttribute);
+    newElement.setAttribute("id", IdAttribute);
+
+    let textNode = document.createTextNode(paramTextNode);
+    newElement.appendChild(textNode);
+    return newElement;
+}
+
+function appendLi(li){
+    let listElement = document.getElementById("list");
+    listElement.appendChild(li);
+}
+
+let liTest = addLi("healthy", 4, "sutmig");
+appendLi(liTest);
